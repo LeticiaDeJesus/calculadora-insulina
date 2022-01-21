f64f321966e7f91566d85608cd08911ed3609108
LeticiaDeJesus <89044907+LeticiaDeJesus@users.noreply.github.com>
2022-01-21 12:11:55 -0300

    ok

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ app = Flask(__name__, static_folder='static')
 def homepage():
     return render_template("index.html")
 
-@app.route("/contatos")
-def contatos():
-    return render_template("contatos.html")
-
 # colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
